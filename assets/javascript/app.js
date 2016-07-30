5568d220b9297296a0a830f004513b32a4d8a6f5
Kai <huikaizhong@yahoo.com>
2016-07-30 17:32:05 -0400

    second update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,22 +1,78 @@
 $(document).ready(function(){
-
-    var button = "setTimeout(timer, 1000*120);"
+	var t = 2 * 60;
+	var interval;
+	var correctAnswers = 0;
+	var inCorrectAnswers = 0;
+	//set a timer
 	function timer() {
-	    
+	    interval = setInterval(function() {
+	    	t--;
+	    	if(t === 0) {
+	    		clearInterval(interval);
+	    		$(".wrap").hide();
+    			$(".last").css("display","block");
+	    		var inputs = $('input[data-correct]:checked');
+	    		correctAnswers = inputs.length;
+	    		inCorrectAnswers = 20 - correctAnswers;
+	    		
+	    		console.log(correctAnswers, inCorrectAnswers);
+	    	}
+	    	$('#seconds-left').text(t);
+	    }, 1000);
 	}
 
-    $(".btn").click(function(){
-        $(".wrap").css("visibility","display");  
 	
-    	$(".wrap").show();
+
+	// click "start" to begin the game
+    $(".start").click(function(){
+        $(".wrap").css("display","block");  
+	
+    	$(".start").hide();
     	timer();
 	});	
 
-    
+    // click "done" to submit the answers and stop the timer
+    $(".submit").click(function(){
+    	clearInterval(interval);
+    	$(".wrap").hide();
+    	$(".last").css("display","block");
+
+    	var checked = $('input:checked')
+    	
+    	$.each(checked, function(index, input) {
+    		console.log(index, input);
+
+    		if($(input).data('correct')) {
+    			correctAnswers++;
+    			console.log('correct answer!')
+    		} else {
+    			inCorrectAnswers++;
+    		}
+    	})
+
+    	console.log(correctAnswers, inCorrectAnswers);
+    });	
+
+    // click "replay" to restart the game without reloading.
+    function reset(){
+		t = 2*60;
+		clearInterval(interval);
+		$(".wrap").css("display","none");  
+	
+    	$(".start").show();
+
+    	$('input:checked').attr('checked', false);
+	}
+
+    $(".replay").click(function(){
+    	reset();
+
+    });
+
+    //create array for answer banks
+    var answers = [];
 
 
-
-
-
+    //loop through 
 
 });

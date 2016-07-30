989e2426a631ef3666f687e3b09258feb8675625
Kai <huikaizhong@yahoo.com>
2016-07-30 14:15:02 -0400

    update

---
+++ assets/javascript/app.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+
+    var button = "setTimeout(timer, 1000*120);"
+	function timer() {
+	    
+	}
+
+    $(".btn").click(function(){
+        $(".wrap").css("visibility","display");  
+	
+    	$(".wrap").show();
+    	timer();
+	});	
+
+    
+
+
+
+
+
+
+});

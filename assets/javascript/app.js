9acc4629ccefc8b60da5e3696c6d77304235a5c0
Kai <huikaizhong@yahoo.com>
2016-07-30 23:54:21 -0400

    midnight update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,21 +1,25 @@
 $(document).ready(function(){
-	var t = 2 * 60;
+
+	//declare variables
+	var t = 120;
 	var interval;
 	var correctAnswers = 0;
-	var inCorrectAnswers = 0;
+	var incorrectAnswers = 0;
+	var unanswered=0;
+
 	//set a timer
 	function timer() {
 	    interval = setInterval(function() {
-	    	t--;
-	    	if(t === 0) {
+	    	t--;	         // count down
+	    	if(t === 0) {    //timeout
 	    		clearInterval(interval);
 	    		$(".wrap").hide();
     			$(".last").css("display","block");
-	    		var inputs = $('input[data-correct]:checked');
+	    		var inputs = $('input[data-correct]:checked');//css selector, check answers. Correct answers have attribute "data-correct"
 	    		correctAnswers = inputs.length;
-	    		inCorrectAnswers = 20 - correctAnswers;
-	    		
-	    		console.log(correctAnswers, inCorrectAnswers);
+	    		incorrectAnswers = 20 - correctAnswers;
+	    		unanswered 
+	    		console.log("Correct Answers: " + correctAnswers, "Incorrect Answers: " + incorrectAnswers, "Unanswered: " + unanswered);
 	    	}
 	    	$('#seconds-left').text(t);
 	    }, 1000);
@@ -23,7 +27,7 @@ $(document).ready(function(){
 
 	
 
-	// click "start" to begin the game
+	// click "start" to begin the game: show content & start timer
     $(".start").click(function(){
         $(".wrap").css("display","block");  
 	
@@ -40,28 +44,43 @@ $(document).ready(function(){
     	var checked = $('input:checked')
     	
     	$.each(checked, function(index, input) {
-    		console.log(index, input);
+    		console.log("index: " + index, "input: " + input);
 
-    		if($(input).data('correct')) {
+    		if($(input).data('correct')) { 
     			correctAnswers++;
     			console.log('correct answer!')
     		} else {
-    			inCorrectAnswers++;
-    		}
-    	})
+    			incorrectAnswers++;
+    		} /*else if {
+    			var items = $('li');
+				$.each(items, function(i, li) {
+  					var inputs =  $(li).find('input:checked');
+  					if(inputs.length === 0) {
+    					unanswered++; 
+  					} */
+  				})
+  			})	
 
-    	console.log(correctAnswers, inCorrectAnswers);
-    });	
+    	
+		
+    	console.log("Correct Answers: " + correctAnswers, "Incorrect Answers: " + incorrectAnswers, "Unanswered: " + unanswered);
+    	$(".correct").html(correctAnswers);
+    	$(".incorrectAnswers").html(incorrectAnswers);
+    	$(".unanswered").html(unanswered);
 
+    	
+ 
     // click "replay" to restart the game without reloading.
     function reset(){
-		t = 2*60;
+		t = 120;
 		clearInterval(interval);
 		$(".wrap").css("display","none");  
 	
     	$(".start").show();
 
-    	$('input:checked').attr('checked', false);
+    	$(".last").css("display","none");
+
+    	$('input:checked').attr('checked', false); //no radio button is chosen
 	}
 
     $(".replay").click(function(){
@@ -69,10 +88,5 @@ $(document).ready(function(){
 
     });
 
-    //create array for answer banks
-    var answers = [];
+    })
 
-
-    //loop through 
-
-});
